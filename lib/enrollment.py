fcb8f2be4d02da016c8ec23d91aa12787a0cb61a
Joyce Njogu <joycenjogu127@gmail.com>
2025-11-28 19:42:05 +0300

    completed lab

--- lib/enrollment.py
+++ lib/enrollment.py
@@ -42,6 +42,13 @@ class Enrollment:
             type(self).all.append(self)
         else:
             raise TypeError("Invalid types for student and/or course")
+@classmethod
+def aggregate_enrollments_per_day(cls):
+    enrollment_count = {}
+    for enrollment in cls.all:
+        date = enrollment.get_enrollment_date().date()
+        enrollment_count[date] = enrollment_count.get(date, 0) + 1
+    return enrollment_count
 
     def get_enrollment_date(self):
         return self._enrollment_date
